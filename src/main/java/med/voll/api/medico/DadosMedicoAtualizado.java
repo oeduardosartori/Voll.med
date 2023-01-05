cd8639fdd4df4a43a54f3b09c3d079dc90dfbc5f
Eduardo <edusartori00@gmail.com>
2023-01-05 11:56:52 -0300

    Configurado return do MedicoController

---
+++ src/main/java/med/voll/api/medico/DadosMedicoAtualizado.java
@@ -0,0 +1,23 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosMedicoAtualizado(
+        Long id,
+        String nome,
+        String email,
+        String crm,
+        String telefone,
+        Especialidade especialidade,
+        Endereco endereco) {
+
+    public DadosMedicoAtualizado(Medico medico){
+        this(medico.getId(),
+                medico.getNome(),
+                medico.getEmail(),
+                medico.getCrm(),
+                medico.getTelefone(),
+                medico.getEspecialidade(),
+                medico.getEndereco());
+    }
+}
