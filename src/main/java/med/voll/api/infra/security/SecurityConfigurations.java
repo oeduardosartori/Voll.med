aeef7116bc867692a0a6b6192a13151a5c6c6c49
Eduardo <edusartori00@gmail.com>
2023-01-12 11:27:52 -0300

    Criado SecurityFilter

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static java.util.GregorianCalendar.BC;
+
 
 @Configuration @EnableWebSecurity
 public class SecurityConfigurations {
